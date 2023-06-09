35bed3041179bdde5cf73223ad1c5d67db6cc19f
Poojalaksh2002 <lakshmipooja232@gmail.com>
2023-06-09 19:47:20 +0530

    component-rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const obj = [
+    {
+      id: 201,
+      header: "Calling Lord",
+      text: "Some message regarding calling lord",
+      timeout: 3000,
+      counter: 3,
+    },
+    {
+      id: 202,
+      header: "Praying",
+      text: "Some message regarding praying",
+      timeout: 5000,
+      counter: 5,
+    },
+    {
+      id: 203,
+      header: "Reading",
+      text: "Some message regarding reading",
+      timeout: 4000,
+      counter: 4,
+    },
+  ];
+  const [count, setCount] = useState(obj[index].counter);
+
+  useEffect(() => {
+    // timeout denotion
+    const timeoutInterval = setTimeout(() => {
+      setIndex((prevIndex) => prevIndex + 1);
+    }, obj[index]?.timeout);
+    let counterInterval;
+    // Counter decreasing logic
+    if (obj[index]) {
+      counterInterval = setTimeout(() => {
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timeoutInterval);
+      if (count === 0) {
+        clearTimeout(counterInterval);
+      }
+    };
+  }, [index, obj]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {obj[index] && (
+        <div>
+          <h1>{obj[index].header}</h1>
+          <p>{obj[index].text}</p>
+          <p>{count}</p>
+        </div>
+      )}
     </div>
   );
 }
